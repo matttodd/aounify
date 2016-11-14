9edfd1fef4628168bc47a4274dc504ba0790858f
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2016-11-14 16:36:11 +0000

    release time

--- content.js
+++ content.js
@@ -1,1 +1,14 @@
-document.getElementsByTagName("body")[0].style.background = "url(http://www.brycethuilot.com/sugar/IMG_0005.jpg)";
+var sugar = false;
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if( request.message === "clicked_browser_action" ) {
+            console.log("sugar: " + sugar);
+            sugar = !sugar;
+            if (sugar) {
+                console.log("sugar time!");
+                document.getElementsByTagName("body")[0].style.background = "url(http://www.brycethuilot.com/sugar/IMG_0005.jpg)";
+            }
+        }
+    }
+);
