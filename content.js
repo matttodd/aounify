08854ba3f5a28b274cd5fc4e2e3e089f35f7843b
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2016-12-19 11:48:51 -0500

    eric j barron

--- content.js
+++ content.js
@@ -3,11 +3,10 @@ var sugar = false;
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if( request.message === "clicked_browser_action" ) {
-            console.log("sugar: " + sugar);
             sugar = !sugar;
             if (sugar) {
-                console.log("sugar time!");
-                document.getElementsByTagName("body")[0].style.background = "url(http://www.brycethuilot.com/sugar/IMG_0005.jpg)";
+                var a = document.getElementsByTagName("img");
+                for (var i = 0; i < a.length; i++) a[i].src = "https://president.psu.edu/images/barron-portrait.jpg";
             }
         }
     }
