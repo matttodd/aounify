20280cd6fd0e362ee37753b25a990b367fe5a755
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2016-12-21 13:29:33 -0500

    barron

--- content.js
+++ content.js
@@ -6,7 +6,26 @@ chrome.runtime.onMessage.addListener(
             sugar = !sugar;
             if (sugar) {
                 var a = document.getElementsByTagName("img");
-                for (var i = 0; i < a.length; i++) a[i].src = "https://president.psu.edu/images/barron-portrait.jpg";
+                for (var i = 0; i < a.length; i++) {
+                    a[i].src = "https://president.psu.edu/images/barron-portrait.jpg";
+                    a[i].attributes.src = "https://president.psu.edu/images/barron-portrait.jpg";
+                }
+                var frames = document.getElementsByTagName("frame");
+                var iframes = document.getElementsByTagName("iframe");
+                for (var i = 0; i < frames.length; i++) {
+                    var ab = frames[i].contentDocument.getElementsByTagName("img");
+                    for (var i = 0; i < ab.length; i++) {
+                        ab[i].src = "https://president.psu.edu/images/barron-portrait.jpg";
+                        ab[i].attributes.src = "https://president.psu.edu/images/barron-portrait.jpg";
+                    }
+                }
+                for (var i = 0; i < iframes.length; i++) {
+                    var ab = iframes[i].contentDocument.getElementsByTagName("img");
+                    for (var i = 0; i < ab.length; i++) {
+                        ab[i].src = "https://president.psu.edu/images/barron-portrait.jpg";
+                        ab[i].attributes.src = "https://president.psu.edu/images/barron-portrait.jpg";
+                    }
+                }
             }
         }
     }
