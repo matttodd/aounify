39ffdf9b8d2f07d4c51b978e451e947a64991346
Matthew Todd <matttodd6@gmail.com>
2017-09-14 21:00:15 -0400

    aoun2

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 var sugar = false;
+var url = "https://rawgit.com/kylesferrazza/barronify/master/aoun.jpg"
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
@@ -7,23 +8,23 @@ chrome.runtime.onMessage.addListener(
             if (sugar) {
                 var a = document.getElementsByTagName("img");
                 for (var i = 0; i < a.length; i++) {
-                    a[i].src = "https://president.psu.edu/images/barron-portrait.jpg";
-                    a[i].attributes.src = "https://president.psu.edu/images/barron-portrait.jpg";
+                    a[i].src = url;
+                    a[i].attributes.src = url;
                 }
                 var frames = document.getElementsByTagName("frame");
                 var iframes = document.getElementsByTagName("iframe");
                 for (var i = 0; i < frames.length; i++) {
                     var ab = frames[i].contentDocument.getElementsByTagName("img");
                     for (var i = 0; i < ab.length; i++) {
-                        ab[i].src = "https://president.psu.edu/images/barron-portrait.jpg";
-                        ab[i].attributes.src = "https://president.psu.edu/images/barron-portrait.jpg";
+                        ab[i].src = url;
+                        ab[i].attributes.src = url;
                     }
                 }
                 for (var i = 0; i < iframes.length; i++) {
                     var ab = iframes[i].contentDocument.getElementsByTagName("img");
                     for (var i = 0; i < ab.length; i++) {
-                        ab[i].src = "https://president.psu.edu/images/barron-portrait.jpg";
-                        ab[i].attributes.src = "https://president.psu.edu/images/barron-portrait.jpg";
+                        ab[i].src = url;
+                        ab[i].attributes.src = url;
                     }
                 }
             }
